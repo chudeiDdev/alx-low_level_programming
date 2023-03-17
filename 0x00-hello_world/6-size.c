c17c7cd6bad8c9537b74e1049a979fdc38490eca
chudeiDdev <nwokoifechukwudei@gmail.com>
2023-03-17 06:29:50 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
-/*
- *You should produce the exact same output as in the example
- *Warnings are allowed
- *Your program should return 0
- *You might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
+/**
+ *main - should print the various types on the computer
+ *Return: 0 if properly exited, non zero if otherwise
 */
 
 int main(void)
 {
-	printf("Size of a char: %d \n", sizeof(char));
-	printf("size of an int: %d \n", sizeof(int));
-	printf("size of a long int: %d \n", sizeof(long int));
-	printf("size of a long long: %d \n", sizeof(long long));
-	printf("size of a float: %d \n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long: %d byte(s)\n", sizeof(long long));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
