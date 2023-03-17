49660ddb7b7e266d52275e555d3532cd0a9f05b4
chudeiDdev <nwokoifechukwudei@gmail.com>
2023-03-17 06:19:49 +0000

    corrected the main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 
-/*
- *main-prints the phrase "Programming is like building a multilingual puzzle
- *Use the function puts
- *You are not allowed to use printf
- *Your program should end with the value 0
- */
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/*
- *main - prints the phrase with proper grammer
- *Use the function printf
- *You are not allowed to use the function puts
- *Your program should return 0
- *Your program should compile without warning when using the -Wall gcc option
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
 */
 
 int main(void)
