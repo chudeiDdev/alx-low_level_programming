4010eb4a9ff0cebfed423ea0ad9b4830da5120d3
chudeiDdev <nwokoifechukwudei@gmail.com>
2023-03-19 16:22:30 +0000

    print combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *Return: 0 succesfull
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+{
+		for (j = i + 1; j < 10; j++)
+{
+			if (i > 0 || j > 1)
+{
+				putchar(',');
+				putchar(' ');
+}
+		putchar(i + '0');
+		putchar(j + '0');
+}
+}
+	putchar('\n');
+	return (0);
+}
